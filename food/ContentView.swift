a6cd03f2175ac930dcd171ac209c3e4e5a920699
khrove <joshualjohnson33@gmail.com>
2023-02-14 17:04:59 -0500

    Initial Commit

---
+++ food/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  food
+//
+//  Created by Joshua Johnson on 2/14/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ food/foodApp.swift
@@ -0,0 +1,17 @@
+//
+//  foodApp.swift
+//  food
+//
+//  Created by Joshua Johnson on 2/14/23.
+//
+
+import SwiftUI
+
+@main
+struct foodApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
