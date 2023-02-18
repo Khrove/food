e8a01fa8699d3d4b5ab4152f272ab4f54fb16d38
khrove <joshualjohnson33@gmail.com>
2023-02-18 09:04:43 -0500

    test

--- food/ViewModels/RecipeModel.swift
+++ food/ViewModels/RecipeModel.swift
@@ -8,41 +8,10 @@
 import Foundation
 
 class RecipeModel: ObservableObject {
-    // Published makese the list, or init function, refresh on list update
-    // Broadcasts changes
     @Published var recipes = [Recipe]()
     
     init() {
-        // Get the path to the json file within the app bundle
-        let pathString = Bundle.main.path(forResource: "data", ofType: "json")
-        
-        if let path = pathString {
-            // Create a url object
-            let url = URL(fileURLWithPath: path)
-            
-            // Error handling
-            do {
-                // Put the code that potentially throws an error
-                // Create a data object with the data at the url
-                let data = try Data(contentsOf: url)
-                
-                // Parse the data
-                let decoder = JSONDecoder()
-                do {
-                    let recipeData = try decoder.decode([Recipe].self, from: data)
-                } catch {
-                    // Could not decode JSON
-                    print(error)
-                }
-            } catch {
-                // Execution will come here if an error is thrown
-                // Handle the error
-                print(error)
-            }
-        }
-    }
-    
-    func addRecipe() {
-        recipes.append(Recipe(name: "Burger", cuisine: "American"))
+        // Parse the local json file
+        self.recipes = DataService.getLocalData()
     }
 }
